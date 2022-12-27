719966ef9f651af35521f73568f2e41fce83e30e
SATISHGARG121175 <104013580+SATISHGARG121175@users.noreply.github.com>
2022-12-27 21:04:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+ function chargerjpg()
+{
+    document.getElementById('charger.jpg');
+}
+function fridgejpg()
+{
+    document.getElementById('fridge.jpg');
+}
+function mousejpg()
+{
+    document.getElementById('mouse.jpg');
+}
+function pillowjpg()
+{
+    document.getElementById('pilow.jpg');
+}
+function tabjpg()
+{
+    document.getElementById('tab.jpg');
+}
---
+++ mouse.js
@@ -0,0 +1,24 @@
+img="";
+status="";
+objects=[];
+function preload() {
+    img=loadImage("mouse.jpg")
+}
+function setup() {
+        canvas=createCanvas(400,309);
+        canvas.center();
+        objectDetector =ml5.objectDetector('cocossd',modelLoaded);
+        document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
